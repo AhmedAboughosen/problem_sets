1f54ce8fa0d652bcd273609d1c6c06d987209fc4
AhmedAboughosen <ahmed.a_naser@outlook.com>
2021-04-25 18:32:35 +0200

    problem from http://codeforces.com/problemset/problem/1353/B

---
+++ src/com/company/TwoArraysSwaps.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoArraysSwaps {
+
+    // problem from http://codeforces.com/problemset/problem/1353/B
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+
+            int n = scanner.nextInt(), k = scanner.nextInt();
+            int lastIndex = n - 1;
+            int firstIndex = 0, sum = 0;
+            int firstArr[] = new int[n];
+            int secondArr[] = new int[n];
+            for (int j = 0; j < n; j++) {
+                firstArr[j] = scanner.nextInt();
+            }
+            for (int j = 0; j < n; j++) {
+                secondArr[j] = scanner.nextInt();
+            }
+
+            Arrays.sort(firstArr);
+            Arrays.sort(secondArr);
+
+            for (int j = 0; j < k; j++) {
+                if (secondArr[lastIndex] > firstArr[firstIndex]) {
+                    firstArr[firstIndex] = secondArr[lastIndex];
+                    lastIndex--;
+                    firstIndex++;
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                sum = sum + firstArr[j];
+            }
+            System.out.println(sum);
+        }
+    }
+}
