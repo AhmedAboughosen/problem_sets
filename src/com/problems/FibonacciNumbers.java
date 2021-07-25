c607a8aeafb63ac2da4621947d259cab9da5533b
AhmedAboughosen <ahmed.a_naser@outlook.com>
2021-07-25 21:36:04 +0200

    learn more about https://en.wikipedia.org/wiki/Fibonacci_number
    Signed-off-by: Ahmed Aboughosen <55014628+AhmedAboughosen@users.noreply.github.com>

---
+++ src/com/problems/FibonacciNumbers.java
@@ -0,0 +1,29 @@
+package com.problems;
+
+public class FibonacciNumbers {
+
+
+    //learn more about https://en.wikipedia.org/wiki/Fibonacci_number
+    public static int computeFibonacciNumbersRecurs(int n) {
+
+        if (n <= 1) return n;
+        return computeFibonacciNumbersRecurs(n - 1) + computeFibonacciNumbersRecurs(n - 2);
+    }
+
+    public static long computeFibonacciNumbersIterative(int n) {
+
+        long sum = 0, A = 0, B = 1;
+        for (int i = 3; i <= n; i++) {
+            sum = A + B;
+            A = B;
+            B = sum;
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(computeFibonacciNumbersRecurs(6-1));
+    }
+}
