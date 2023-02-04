e5e28982a5f9ef86b0c753e310e42e7c10fdde1b
Ahmed Aboughosen <55014628+AhmedAboughosen@users.noreply.github.com>
2023-02-04 20:13:53 +0200

    set  of examples for recusrsion
    Signed-off-by: Ahmed Aboughosen <55014628+AhmedAboughosen@users.noreply.github.com>

--- src/com/problems/recursion/BasicExamples.java
+++ src/com/problems/recursion/BasicExamples.java
@@ -8,9 +8,29 @@ public class BasicExamples {
 //        printPreFix(5);
 //        System.out.println(pow(7, 3));
 
-        int arr[] = new int[]{7,10,23,52,15,14,1};
+        int arr[] = new int[]{7, 10, 23, 52, 15, 14, 1};
 
-        System.out.println(maxValue(arr,0));
+//        System.out.println(maxValue(arr,0));
+//        System.out.println(avgArr(new int[]{1,8,2,10,3},5));
+
+//        var newArr = arrayIncrement(new int[]{1, 8, 2, 10, 3}, 5);
+//        var outPutedArray = new int[]{1, 8, 2, 10, 3};
+//
+//        arrayAccoumulation(outPutedArray, 5);
+//
+//        for (int i = 0; i < outPutedArray.length; i++) {
+//            System.out.println(outPutedArray[i]);
+//        }
+
+        var outPutedArray = new int[]{1, 3, 5, 7, 4,2};
+
+        leftMax(outPutedArray, 6);
+
+        for (int i = 0; i < outPutedArray.length; i++) {
+            System.out.println(outPutedArray[i]);
+        }
+
+//        System.out.println(arrayIncrement(new int[]{1,8,2,10,3},5));
     }
 
 
@@ -49,5 +69,41 @@ public class BasicExamples {
         }
 
         return Math.max(arr[index], maxValue(arr, index + 1));
+    }
+
+    public static double avgArr(int arr[], int len) {
+
+        if (len == 1) return arr[0];
+
+        return (arr[len - 1] + avgArr(arr, len - 1)) / 2;
+    }
+
+    public static int[] arrayIncrement(int arr[], int len) {
+
+        if (len == 1) return arr;
+
+        arrayIncrement(arr, len - 1);
+
+        arr[len - 1] = arr[len - 1] + len - 1;
+        return arr;
+    }
+
+    public static int arrayAccoumulation(int arr[], int len) {
+
+        if (len == 1) return 1;
+
+
+        arr[len - 1] = arrayAccoumulation(arr, len - 1) + arr[len - 1];
+        return arr[len - 1];
+    }
+
+    public static int leftMax(int arr[], int len) {
+
+        if (len == 1) return arr[0];
+
+
+
+        arr[len - 1] = Math.max(leftMax(arr, len - 1) , arr[len - 1]);
+        return arr[len - 1];
     }
 }
